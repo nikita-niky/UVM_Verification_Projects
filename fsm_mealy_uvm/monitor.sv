7b02b88d406b106200b055d77364b814fa92f312
Nikita A. <nikita.niky.agrawal@gmail.com>
2026-04-10 09:36:38 +0000

    Adding FSM Mealy state machine UVM project

---
+++ fsm_mealy_uvm/monitor.sv
@@ -0,0 +1,53 @@
+class fsm_monitor extends uvm_monitor;
+  `uvm_component_utils(fsm_monitor)
+
+  virtual fsm_if vif;
+  fsm_item tr;
+
+  uvm_analysis_port #(fsm_item) send;
+
+  function new(string name = "fsm_monitor", uvm_component parent);
+    super.new(name, parent);
+    send = new("send", this);
+  endfunction
+
+  virtual function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    if(!uvm_config_db#(virtual fsm_if)::get(this, "", "vif", vif))
+      `uvm_error("NOVIF", {"virtual interface must be set for: ", get_full_name(), ".vif"});
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+   
+    forever begin
+      tr = fsm_item::type_id::create("tr");
+      
+      @(vif.mon_cb);
+      
+      if(vif.mon_cb.rst_n) begin
+
+        tr.rst_n          = vif.mon_cb.rst_n;
+        tr.bit_in         = vif.mon_cb.bit_in;
+        tr.pattern_found  = vif.mon_cb.pattern_found;
+        tr.current_state  = state_t'(vif.mon_cb.current_state);
+
+      `uvm_info("MON", $sformatf("rst_n=%0b | Sampled Bit: %b, State: %s, Found: %b",tr.rst_n,tr.bit_in, tr.current_state.name(), tr.pattern_found), UVM_LOW)     
+     
+       send.write(tr);
+      
+    end
+      else begin
+        `uvm_info("MON","negedge Reset ACTIVE", UVM_LOW)
+        
+        tr.rst_n          = vif.mon_cb.rst_n;
+        tr.bit_in         = vif.mon_cb.bit_in;
+        tr.pattern_found  = vif.mon_cb.pattern_found;
+        tr.current_state  = state_t'(vif.mon_cb.current_state);
+
+        `uvm_info("MON", $sformatf("rst_n=%0b | Sampled Bit: %b, State: %s, Found: %b",tr.rst_n,tr.bit_in, tr.current_state.name(), tr.pattern_found), UVM_LOW)   
+        send.write(tr);
+
+      end
+    end
+  endtask
+endclass
---
+++ fsm_mealy_uvm/tb_top.sv
@@ -0,0 +1,44 @@
+`include "interface.sv"
+`include "fsm_pkg.sv"
+`include "assertion.sv"
+
+module tb_top;
+  import uvm_pkg::*;
+  import fsm_pkg::*;
+
+  // Clock and Reset Signals
+  logic clk;
+
+ 
+  initial begin
+   clk = 0;
+    forever #(5.0) clk = ~clk;
+  end
+
+  fsm_if fif(clk); 
+  
+  fsm_mealy dut (
+    .clk(fif.clk),
+    .rst_n(fif.rst_n),
+    .bit_in(fif.bit_in),
+    .pattern_found(fif.pattern_found)
+  );
+  
+ 
+    assign fif.current_state = dut.current_state;
+  
+  initial begin
+    // Set the virtual interface in the Config DB
+    uvm_config_db#(virtual fsm_if)::set(null, "uvm_test_top.*", "vif", fif);
+
+    // Run the test
+    run_test("fsm_test");
+  end
+
+  // Waveform dump
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,tb_top);
+  end
+
+endmodule
