7d9ce51c5df58ec9e18f66c20e4feaa7d6d81985
Nikita A. <nikita.niky.agrawal@gmail.com>
2026-04-14 15:34:14 +0000

    Remove old arbiter directories after consolidating into arbiter_uvm

--- arbiter_fp_uvm/sequencer.sv
+++
@@ -1,7 +0,0 @@
-class arbiter_sequencer extends uvm_sequencer #(arbiter_item);
-  `uvm_component_utils(arbiter_sequencer)
-  
-  function new(string name = "arbiter_sequencer", uvm_component parent);
-    super.new(name, parent);
-  endfunction
-endclass
