e21e0b1cfadeec8d313c853488b2e4627c613ecb
Nikita A. <nikita.niky.agrawal@gmail.com>
2026-04-14 15:31:02 +0000

    Remove old FSM directories after consolidating into fsm_uvm

--- fsm_moore_uvm/sequencer.sv
+++
@@ -1,7 +0,0 @@
-class fsm_sequencer extends uvm_sequencer #(fsm_item);
-  `uvm_component_utils(fsm_sequencer)
-
-  function new(string name = "fsm_sequencer", uvm_component parent);
-    super.new(name, parent);
-  endfunction
-endclass
